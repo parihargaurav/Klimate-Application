b937bf1f2197594b28891771bed2c27c7bfe6c37
parihargaurav <parihargaurav527@gmail.com>
2025-09-28 17:11:43 +0530

    klimate application

---
+++ src/api/weather.ts
@@ -0,0 +1,73 @@
+// api endpoints service.
+
+import { API_CONFIG } from "./config";
+import type {
+  Coordinates,
+  ForecastData,
+  GeocodingResponse,
+  WeatherData,
+} from "./types";
+
+class WeatherAPI {
+  private createUrl(
+    endpoint: string,
+    //key = string,   value = number. or string
+    params: Record<string, string | number>
+  ) {
+    const searchParams = new URLSearchParams({
+      appid: API_CONFIG.API_KEY,
+      ...params,
+    });
+    return `${endpoint}?${searchParams.toString()}`;
+  }
+
+  // here whenever we don't know any type we write as <T>
+  //   Type Safety → Using <T> with TypeScript ensures the response data matches the expected format for each endpoint.
+  private async fetchData<T>(url: string): Promise<T> {
+    const response = await fetch(url);
+    if (!response.ok)
+      throw new Error(`Weather API Error:${response.statusText}`);
+    return response.json();
+  }
+
+  async getCurrentWeather({ lat, lon }: Coordinates): Promise<WeatherData> {
+    const url = this.createUrl(`${API_CONFIG.BASE_URL}/weather`, {
+      lat: lat.toString(),
+      lon: lon.toString(),
+      units: API_CONFIG.DEFAULT_PARAMS.units,
+    });
+    return this.fetchData<WeatherData>(url);
+  }
+
+  async getForecast({ lat, lon }: Coordinates): Promise<ForecastData> {
+    const url = this.createUrl(`${API_CONFIG.BASE_URL}/forecast`, {
+      lat: lat.toString(),
+      lon: lon.toString(),
+      units: API_CONFIG.DEFAULT_PARAMS.units,
+    });
+    return this.fetchData<ForecastData>(url);
+  }
+  async reverseGeocode({
+    lat,
+    lon,
+  }: Coordinates): Promise<GeocodingResponse[]> {
+    const url = this.createUrl(`${API_CONFIG.GEO}/reverse`, {
+      lat: lat.toString(),
+      lon: lon.toString(),
+      limit: "1",
+    });
+    return this.fetchData<GeocodingResponse[]>(url);
+  }
+  async searchLocations(query: string): Promise<GeocodingResponse[]> {
+    const url = this.createUrl(`${API_CONFIG.GEO}/direct`, {
+      q: query,
+      limit: "5",
+    });
+    return this.fetchData<GeocodingResponse[]>(url);
+  }
+}
+
+
+
+
+export const weatherAPI = new WeatherAPI();
